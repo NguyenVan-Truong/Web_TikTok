a25f5176466176055bf48d0804f5610d80b19a01
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2025-04-16 23:01:48 +0700

    pull

--- fe/src/layouts/default.tsx
+++ fe/src/layouts/default.tsx
@@ -13,16 +13,49 @@ export default function DefaultLayout({
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://heroui.com"
-          title="heroui.com homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">HeroUI</p>
-        </Link>
+      <footer className="bg-black w-full text-white fixed bottom-0 left-0 h-16 flex justify-around items-center z-50">
+        <nav>
+          <ul className="flex justify-around items-center gap-4 space-x-9">
+            <Link
+              href="/"
+              className="flex flex-col text-white hover:text-gray-200 "
+            >
+              <i className="fa-solid fa-house text-2xl"></i>
+              <span className="text-xs"> Home</span>
+            </Link>
+            <Link
+              href="/discover"
+              className="flex flex-col text-white hover:text-gray-200 text-xs"
+            >
+              <i className="fa-solid fa-magnifying-glass text-2xl"></i>
+              <span className="text-xs"> Discover</span>
+            </Link>
+            <Link
+              href="/add"
+              className="flex flex-col text-white hover:text-gray-200 "
+            >
+              <img
+                src="https://res.cloudinary.com/dcvu7e7ps/image/upload/v1744818763/Button_Shape_s8fj3r.svg"
+                alt="https://res.cloudinary.com/dcvu7e7ps/image/upload/v1744818763/Button_Shape_s8fj3r.svg"
+              />
+            </Link>
+            <Link
+              href="/inbox"
+              className="flex flex-col text-white hover:text-gray-200 text-xs"
+            >
+              {" "}
+              <i className="fa-regular fa-comment  text-2xl"></i>
+              <span className="text-xs"> Inbox</span>
+            </Link>
+            <Link
+              href="/Me"
+              className="flex flex-col text-white hover:text-gray-200 text-xs"
+            >
+              <i className="fa-regular fa-user  text-2xl"></i>
+              <span className="text-xs"> Me</span>
+            </Link>
+          </ul>
+        </nav>
       </footer>
     </div>
   );
